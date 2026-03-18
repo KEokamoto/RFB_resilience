d49ad82b5fb816c2c849d273447cd239216a5540
Okamoto <okamoto@caltech.edu>
2026-03-18 16:12:06 -0700

    Load required packages and define global variables

--- scripts/00_setup.R
+++ scripts/00_setup.R
@@ -1,1 +1,18 @@
-# Load packages and define global variables
+# Load required packages
+library(tidyverse)
+library(phyloseq)
+library(vegan)
+library(readxl)
+library(RColorBrewer)
+library(patchwork)
+library(DESeq2)
+library(pheatmap)
+library(scales)
+
+# Define global variables
+gen_levels <- c("G0","G1","G2","G3")
+gen_cols <- brewer.pal(4, "Set1")
+names(gen_cols) <- gen_levels
+
+trt_levels <- c("ControlA","ExpA","ControlB","ExpB")
+trt_shapes <- c("ControlA"=16, "ExpA"=17, "ControlB"=1, "ExpB"=2)

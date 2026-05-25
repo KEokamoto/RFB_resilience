a3d19c736879b4fa587786c9b4f1a8f1b67b4528
Okamoto <okamoto@caltech.edu>
2026-05-24 20:58:43 -0400

    Add new analysis and figures scripts to run_all.R

--- scripts/run_all.R
+++ scripts/run_all.R
@@ -1,4 +1,7 @@
 source("scripts/00_setup.R")
 source("scripts/01_processing.R")
 source("scripts/02_analysis.R")
+source("scripts/02_analysis_integrated.R")
 source("scripts/03_figures.R")
+source("scripts/03_figure_w_supp.R")
+source("scripts/03_figures_integrated.R")

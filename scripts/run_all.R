afd6949de4f30c27b10c39f4c22a4992c341cf7f
Okamoto <okamoto@caltech.edu>
2026-06-29 18:19:19 -0400

    Update run_all.R to source integrated analysis script
    Removed obsolete source calls for analysis scripts.

--- scripts/run_all.R
+++ scripts/run_all.R
@@ -1,7 +1,4 @@
 source("scripts/00_setup.R")
 source("scripts/01_processing.R")
-source("scripts/02_analysis.R")
 source("scripts/02_analysis_integrated.R")
-source("scripts/03_figures.R")
-source("scripts/03_figure_w_supp.R")
 source("scripts/03_figures_integrated.R")
